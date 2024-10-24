4505cbb78ec65f3fb686deb78aa18b5571af3fef
azanra <112457939+azanra@users.noreply.github.com>
2024-10-24 09:45:05 +0700

    Hide description when button is clicked

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import "./styles.css";
+import { useState } from "react";
 
+function ButtonElement({description}) {
+  const [displayDescription, setDisplayDescription] = useState(false);
+  function displayElement() {
+      setDisplayDescription(!displayDescription);
+  }
+  return (
+    <>
+        <button onClick={displayElement}>See description</button>
+        {displayDescription ? <DescriptionElement style={{opacity: 1}} description={description}/> : null}
+    </>
+  )
+}
+
+function DescriptionElement({description}) {
+  return (
+    <>
+    <p className="description">{description}</p>
+    </>
+  )
+}
 
 export default function ListingAd({ pic, title, address, description }) {
+
   return (
     <div className="App">
       <img className="mainPic" width="300" height="500" src={pic} />
       <div className="mainContent">
         <h1>{title}</h1>
         <p className="address">{address}</p>
-        <button>See description</button>
-        <p className="description">{description}</p>
+        <ButtonElement description={description}/>
       </div>
     </div>
   );
